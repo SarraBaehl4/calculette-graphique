3ef6b7040c5bb79c2fdf0d6bada5c085285ccac8
baehl <baehl44@yahoo.fr>
2025-01-29 16:14:34 +0100

    modification des boutons de la calculette-G

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//creation de fonction qui récupère la valeur de l'input d'affichage
+// cet input va récupérer la valeur de chaque bouton de la calculette
 function getValue(input){
     document.getElementById("result").value += input
+}
+
+//creation de fonction qui va calculer la somme de 2 inputs
+function SumInputs(){
+   let result = document.getElementById("result").value
+   document.getElementById("result").value = eval(result)
 }
