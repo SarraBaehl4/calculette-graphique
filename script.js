0910852655411b718e5aca649c85c99ff4286162
baehl <baehl44@yahoo.fr>
2025-01-29 16:22:50 +0100

    calculette fonctionnelle addition soustra multiplication et division

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ function getValue(input){
     document.getElementById("result").value += input
 }
 
-//creation de fonction qui va calculer la somme de 2 inputs
-function SumInputs(){
+//creation de fonction qui va calculer la somme, soustraction, multiplication et division de 2 inputs
+function operationOfInputs(){
    let result = document.getElementById("result").value
    document.getElementById("result").value = eval(result)
+}
+
+//creation de fonction pour vider l'afficheur de la calculette:
+function cleanDisplay(){
+    document.getElementById("result").value= ''
 }
